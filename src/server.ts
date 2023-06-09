65ef5025ea2806e9058eb7b05bff1a50dd720012
duki <duki.dodier@gmail.com>
2023-06-09 19:24:06 -0300

    navbar criada

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import path from "path";
 import mustache from "mustache-express";
 import mainRoutes from "./routes/mainRoutes";
-import connection from "./db/conn";
 
 dotenv.config();
 
@@ -14,12 +13,16 @@ server.set("view engine", "mustache");
 server.set("views", path.join(__dirname, "views"));
 server.engine("mustache", mustache());
 
+
+// Configuração do middleware para servir arquivos estáticos da pasta 'public'
+server.use(express.static('public'));
+
 //processar dados enviados por meio de formulários HTML com o método POST
 server.use(express.urlencoded({ extended: true }));
 
 //rotas
 server.use(mainRoutes);
-connection
+
 
 server.listen(process.env.PORT, () => {
   console.log("✅server listening on port " + process.env.PORT);
